8074162648390bef5c40118be4f74fa5844cde34
gstamb16 <george.stamb16@gmail.com>
2026-01-02 12:39:17 +0300

    test env utils

--- tests.lua
+++ tests.lua
@@ -1,29 +1,60 @@
 local info = debug.getinfo(1, "S")
 local script_path = info.source:match("@(.+)[/\\]")
 
+package.path =
+  script_path .. "/utils/?.lua;" ..
+  package.path
 
-package.path = script_path .. "/?.lua;" .. package.path
-
-local Track = require("track")
-
+local EnvUtils = require("env_utils")
 
 local function begin_edit()
-    reaper.Undo_BeginBlock()
-    reaper.PreventUIRefresh(1)
+  reaper.Undo_BeginBlock()
+  reaper.PreventUIRefresh(1)
 end
 
 local function end_edit(desc)
-    reaper.PreventUIRefresh(-1)
-    reaper.Undo_EndBlock(desc, -1)
+  reaper.PreventUIRefresh(-1)
+  reaper.Undo_EndBlock(desc, -1)
+end
+
+local ts_start, ts_end = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
+if ts_start >= ts_end then
+  reaper.ShowMessageBox("No time selection. Create a time selection first.", "8.D", 0)
+  return
+end
+
+local retval, trackidx, itemidx, takeidx, fxidx, parm = reaper.GetTouchedOrFocusedFX(0)
+if not retval then
+  reaper.ShowMessageBox("No last-touched FX parameter.\nTouch a knob, then run again.", "8.D", 0)
+  return
+end
+
+if itemidx ~= -1 then
+  reaper.ShowMessageBox("Take FX not supported in this script.\nTouch a Track FX knob.", "8.D", 0)
+  return
+end
+
+local track = (trackidx == -1) and reaper.GetMasterTrack(0) or reaper.GetTrack(0, trackidx)
+if not track then
+  reaper.ShowMessageBox("Could not resolve track.", "8.D", 0)
+  return
 end
 
 begin_edit()
-reaper.ClearConsole()
 
-local track = Track.new(0, 0)
+local env = reaper.GetFXEnvelope(track, fxidx, parm, true)
+if not env then
+  end_edit("8.D: Replace envelope points")
+  reaper.ShowMessageBox("Failed to get/create FX envelope.", "8.D", 0)
+  return
+end
 
-reaper.ShowConsoleMsg(Track.get_fx_string_repr(track))
+-- Example: write a linear ramp 0 -> 1 over time selection
+local points = {
+  { time = ts_start, value = 0.0, shape = EnvUtils.SHAPE.SQUARE, tension = 0.0, selected = false },
+  { time = ts_end,   value = 1.0, shape = EnvUtils.SHAPE.LINEAR, tension = 0.0, selected = false }
+}
 
+EnvUtils.replace_points_in_range(env, ts_start, ts_end, points)
 
-
-end_edit("test fx")
+end_edit("8.D: Replace envelope points in time selection")
